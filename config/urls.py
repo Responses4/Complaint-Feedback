089f46d42ba6f3f794aa7ebe60dfb9b7e7513567
Neila16 <240625@astanait.edu.kz>
2026-01-28 14:08:39 +0500

    assignment 8

--- config/urls.py
+++ config/urls.py
@@ -3,5 +3,5 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('complaint/', include('complaints.urls')),
+    path('', include('complaints.urls')),  
 ]
