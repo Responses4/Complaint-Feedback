20a9f70c841540da25b8165a96f8225fab60eb83
Neila16 <240625@astanait.edu.kz>
2026-02-06 14:31:02 +0500

    +detail

--- complaints/urls.py
+++ complaints/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from .views import complaint_form, complaint_list, complaint_api, success, complaint_detail_api
+from .views import complaint_form, complaint_list, complaint_api, success, complaint_detail_api, complaint_detail
 
 urlpatterns = [
     path('', complaint_form, name='complaint_form'),
@@ -7,4 +7,5 @@ urlpatterns = [
     path('api/', complaint_api, name='complaint_api'),
     path('api/<int:pk>/', complaint_detail_api, name='complaint_detail_api'), 
     path('success/', success, name='success'),
+    path('complaints/<int:pk>/', complaint_detail, name='complaint_detail'),
 ]
